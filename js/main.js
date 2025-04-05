a052d08eff3b442c8289a4f67ab2a4c94d7120a7
daisykroyter <nubchenko@gmail.com>
2025-04-05 17:55:42 +0300

    added pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-var productSwiper = new Swiper(".product__swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  effect: "fade",
+document.querySelectorAll(".tab").forEach((tab) => {
+  tab.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(".tab--active")?.classList.remove("tab--active");
+    this.classList.add("tab--active");
+
+    document
+      .querySelector(".tabs-content--active")
+      ?.classList.remove("tabs-content--active");
+    const targetId = this.getAttribute("href").substring(1);
+    document.getElementById(targetId)?.classList.add("tabs-content--active");
+  });
 });
 
 function fixMobileVh() {
